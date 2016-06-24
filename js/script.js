ecf32a913291f1bfd025758a542b0280f72e3dd5
unknown <272964081@qq.com>
2016-06-24 14:57:58 +0800

    新创建了一个页面，单是不是很很好

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ $(function(){
 		'src':'img/P0'+ran+'.jpg'
 	});
 	$('#main').css({
-		'height':$(window).height()+'px',
+		'height':$(window).height()-$('.title').eq(0).innerHeight()*2+'px',
 	});
 	$('#mypic').mouseover(function(){
 		$pic.attr('src','img/P06.png');
+		$('body').css('background-color','#000');
 	})
 	$('#mypic').mouseout(function(){
 		$pic.attr('src','img/P0'+ran+'.jpg');
+		$('body').css('background-color','');
 	})
 })
---
+++ js/script_pre.js
@@ -0,0 +1,6 @@
+$(function(){
+	// $('#main').css({
+	// 	'height':$(window).height(),
+	// 	'width':$(window).width()
+	// })
+})
