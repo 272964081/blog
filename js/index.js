6a138df7abe174fe6839cd8e7634a30767f7ef5f
unknown <272964081@qq.com>
2016-08-05 18:54:42 +0800

    实现了图片自动轮播

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@ $(function(){
 	//1.更新系统时间
 	upDateTimes();
 	setInterval('upDateTimes()',1000);
+	//2.添加点击事件
+	var $box = $(".pic-box");
+	var $boxUl = $box.find("ul");
+	var $btnGroup =$box.find(".btn-group a");
+	$btnGroup.each(function(i,e){
+		var oLeft = -640*i+'px';
+		$(this).click(function(){
+			$btnGroup.removeClass("active");
+			$boxUl.css("left",oLeft);
+			$(this).addClass("active");
+			if(int){
+				removeInterval(int);
+				picRoll($boxUl,3000);
+			}
+		});
+	});
+	//3.图片自动转换
+	picRoll($boxUl,3000);
 	
 });
 
@@ -23,4 +41,27 @@ function upDateTimes(){
 //个位数补零操作
 function number(num){
 	return (num<10)?('0'+num):num;
+}
+
+//图片自动轮播
+var int;
+function picRoll(ele,delay){
+	var $box = ele;
+	var $btng =$(".pic-box").find(".btn-group a");
+	 int= setInterval(function(){
+	 	var current = $box.css("left");
+	 	var mleft ;
+	 	if (current==='0px') {
+	 		mleft='-640px';
+	 		$btng.removeClass('active').eq(1).addClass('active');
+	 	}else if(current==='-640px'){
+	 		mleft='-1280px';
+	 		$btng.removeClass('active').eq(2).addClass('active');
+	 	}else if(current==='-1280px'){
+	 		mleft='0px';
+	 		$btng.removeClass('active').eq(0).addClass('active');
+	 	}
+	 	$box.css("left",mleft);
+		
+	},delay);
 }
